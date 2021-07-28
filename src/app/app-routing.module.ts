09a2919a851a5cb07491282803d0365c68edbd18
Ana Oliveira <vianello.ana@gmail.com>
2021-07-28 16:39:01 -0300

    Atualizando Pag sobre com menu do início

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component: InicioComponent},
+{path:'sobre', component: SobreComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
