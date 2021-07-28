1265575b216ba50a73142ec2b247241a8fd03567
Ícaro <allegarcezz@gmail.com>
2021-07-28 12:02:00 -0300

    Adicionando componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BarrauserComponent } from './barrauser/barrauser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    SobreComponent,
+    InicioComponent,
+    BarrauserComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/barrauser/barrauser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barrauser',
+  templateUrl: './barrauser.component.html',
+  styleUrls: ['./barrauser.component.css']
+})
+export class BarrauserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
